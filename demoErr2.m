b9084e671c7052994d49326b45d699c553c056d9
skiloop <skiloop@126.com>
2013-04-13 13:27:39 +0800

    adding matlab scripts

---
+++ demoErr2.m
@@ -0,0 +1,41 @@
+%% load data
+% load new dat
+cd /home/skiloop/workspace/hpw3d/new
+loadAllDat
+cd /home/skiloop/matlab/hpw3d
+save new
+clear
+% load old dat
+cd /home/skiloop/workspace/hpw3d/old
+loadAllDat
+cd /home/skiloop/matlab/hpw3d
+save old
+clear
+% load npml and opml
+load old opml
+load new npml
+%% compare
+% compute analysis results
+amp=1e-10;
+tw=2e-9;
+t0 = 4.0 * tw;
+C = 2.99792458E8;
+Pi = 3.14159265358979;
+mu=4.0*Pi*1.0e-7;
+dx=50e-3;
+dy=50e-3;
+dz=50e-3;
+
+dt = 0.99 / (C * sqrt(1.0 / (dx * dx) + 1.0 / (dy * dy) + 1.0 / (dz * dz)));
+len=1:500;
+dist=12*dx;
+delay=dist/C;close
+t=len*dt;
+% numeric solve
+yreal=fun(t-delay,dist,amp,t0,tw,mu,C);
+%yreal=yreal*0.1317/max(yreal);
+
+%
+figure('Name','ErrorComparision');
+plot(t,yreal,'r',t,npml,'b+',t,opml,'k.-');
+legend('numeric','npml','opml');
