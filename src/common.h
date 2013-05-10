fba32e6175aeaff19ddbb308b3cda7990202c904
skiloop <skiloop@126.com>
2013-05-10 12:32:20 +0800

    set default N0 zero

--- src/common.h
+++ src/common.h
@@ -31,7 +31,7 @@ typedef pMyDataF* ppMyDataF;
 #define DEFAULT_NU_FORMAT ALI
 
 // default value for max density
-#define DEFAULT_DENSITY_MAX (1e7)
+#define DEFAULT_DENSITY_MAX (0)
 
 // some common constants
 const MyDataF C = 2.99792458E8; // speed of light in free space
